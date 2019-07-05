ccbebd95301e0d6b54965cc39c837e784e8a2b57
Ipsedo <berriens@gmail.com>
2019-07-05 16:30:46 +0200

    test model wrapped in function

--- user_test_dbpedia.py
+++ user_test_dbpedia.py
@@ -3,7 +3,8 @@ from models import ConvModelDBPedia_V1
 import pickle as pkl
 from doc import __padding__, process_doc, pass_to_idx_and_padd
 
-if __name__ == "__main__":
+
+def test():
     model_file_name = ""
     vocab_file_name = ""
     class_to_idx_file_name = ""
@@ -30,3 +31,7 @@ if __name__ == "__main__":
     predicted = out.argmax(dim=-1).view(-1).item()
 
     print("Predicted : %s with %f probability" % (idx_to_class[predicted], out.view(-1)[predicted].item()))
+
+
+if __name__ == "__main__":
+    test()
